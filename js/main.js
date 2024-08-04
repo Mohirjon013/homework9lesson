841649a8cde15db6aa60eea34cbe780a4e577d47
Mohirjon013 <mohirjon26@gmail.com>
2024-08-04 08:00:41 +0100

    commit

--- js/main.js
+++ js/main.js
@@ -1,4 +1,126 @@
 // Homework 
+
+// 1-rasm
+// let arr = [55,3,2,11,3,4,6,7,12]
+// console.log(arr.sort((a,b) => a-b));
+
+// 2-task
+// let arr = [1,6,9,5,8,10,15]
+// function rangeSum(arr, startIndex, endIndex){
+//     let evenRes = 0
+//     const res = arr.slice(startIndex, endIndex + 1)
+//     res.forEach(value => evenRes += value);
+//     return evenRes
+// }
+// console.log(rangeSum(arr, 2 , 5));
+
+// 3-task
+// let arr = [2,4,5,6,6,3,2,1]
+// let all = []
+// const res = arr.filter(value => {
+//     if(!all.includes(value)){
+//         all.push(value)
+//         return all
+//     }
+// })
+// console.log(res);
+
+// 4-task
+// let arr = [10, 2, 3, 4, 5, 6, 8, 1022];
+// function findMaxMin(array){
+//     let min = array[0]
+//     let max = array[0]
+//     array.filter(value => {
+//         if(value < min){
+//             min = value
+//         }
+//         else{
+//             max = value
+//         }
+//     });
+//     console.log(`MAX: ${max}`);
+//     console.log(`MIN: ${min}`);
+// }
+// findMaxMin(arr)
+
+// 5-task
+// let arr = [1,5,8,9,10]
+// let firstNum = arr[0]
+// arr.shift()
+// arr.push(firstNum)
+// console.log(arr);
+
+// 6-task
+// let arr = [1,2,3,5,6,8]
+// const res = arr.map(value => value)
+// console.log(res.reverse());
+
+// 7-task
+// let arr = [5,4,36,7,8,3,2,9,6,1]
+// const res = arr.filter(value => {
+//     if(value % 2 != 0){
+//         return value
+//     }
+// })
+// console.log(res);
+
+// 8-task
+// let arr = [2,3,4,5,7,6]
+// const res = arr.filter((value, index, arr) => index % 2 != 0)
+// console.log(res);
+
+// 9-task
+// let arr = [2,3,4,25,7,35]
+// let max = [0]
+// function findMax(arr){
+//     arr.forEach((value, index, arr) => {
+//         if(index % 2 != 0){
+//             if(max < value){
+//                 max = value
+//             }
+//         }
+//     })
+//     console.log(max);
+// }
+// findMax(arr)
+
+// 11-task
+// let arr = [1,2,3,4,5,6,7]
+// const res = arr.filter(value => value % 2 != 0)
+// console.log(res);
+
+// 12-task
+// let arr = [22,5,6,3,2]
+// let all = []
+// const res = arr.filter(value => {
+//     all.unshift(value)
+// })
+// console.log(all);
+
+// 13-task
+// let arr = [2,4,5,5,6,6,3,2,2]
+// let all = []
+// const res = arr.filter(value => {
+//     if(!all.includes(value)){
+//         all.push(value)
+//         return all
+//     }
+// })
+// console.log(res.sort((a, b) => a - b));
+
+// 14-task
+// let arr = [10,1,2,3,4,5,6]
+// let min = arr[0]
+// arr.filter((value, index, arr) => {
+//     if(index % 2 == 0){
+//         if(min > value){
+//             min = value
+//         }
+//     }
+// })
+// console.log(min);
+
+
 // 2-rasm
 
 // 1-task
@@ -125,23 +247,6 @@
 //     }
 // })
 // console.log(min);
-
-// 6-task
-// let sizez = prompt("enter size") - 0
-// let arr = []
-// function enterObj(size){
-//     let value = prompt("enter value")
-//     arr.push(value)
-//     let newValue = size - 1
-//     if(newValue > 0){
-//         enterObj(newValue)
-//     }
-// }
-// console.log(enterObj(sizez)
-// );
-// console.log(arr);
-
-
 
 // 7-task
 // let workersList = [
